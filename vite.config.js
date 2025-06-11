0cd6f80974cc850b664797947e8ab0cb2963f4c9
Neel <neeldiwani@gmail.com>
2025-06-11 21:38:35 +0530

    feat: update package.json and vite.config.js for deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/E_commerceUI/",
   plugins: [react(), tailwindcss()],
-  // base: "/E_commerceUI",
 });
